193bc74a6b047f68ffb563bb831a1dd8ab9af072
Raphaël Barrois <raphael.barrois@polyconseil.fr>
2019-12-19 18:52:06 +0100

    Test more complex Django forms.

--- tests/test_django.py
+++ tests/test_django.py
@@ -2,8 +2,6 @@
 # Copyright (c) The cliform project
 # This code is distributed under the two-clause BSD License.
 
-import typing as T
-
 from django import forms
 
 import cliform
@@ -17,6 +15,16 @@ class SimpleForm(forms.Form):
     email = forms.EmailField(label="Email")
 
 
+class ComplexForm(forms.Form):
+    name = forms.CharField(label="Name")
+    email = forms.EmailField(label="Email")
+    is_staff = forms.BooleanField(label="Is Staff")
+    is_superuser = forms.BooleanField(required=False)
+    profile = forms.ChoiceField(choices=[
+        ('intern', "Intern"),
+        ('fulltime', "Full-Time"),
+        ('contractor', "Contractor"),
+    ], required=False)
 
 
 class SimpleFormTests(utils.InteractionTestCase):
@@ -79,4 +87,52 @@ class SimpleFormTests(utils.InteractionTestCase):
         self.assertEqual({
             'email': 'john.doe@example.com',
             'name': "John Doe",
+        }, self.prompter.data)
+
+
+class ComplexFormTests(utils.InteractionTestCase):
+    def setUp(self):
+        class FormPrompter(cliform.django.FormPrompter):
+            form_class = ComplexForm
+            data = None
+
+            def on_submit(self, data):
+                self.data = data
+
+        self.prompter = FormPrompter()
+
+    def test_nominal(self) -> None:
+        self.assertSequence(
+            self.prompter,
+            [
+                utils.ExpectMsg(">>> Name?"),
+                utils.ExpectQuery(reply="John Doe"),
+                utils.ExpectMsg(">>> Email?"),
+                utils.ExpectQuery(reply="john.doe@example.com"),
+                utils.ExpectMsg(">>> Is Staff? ([Y]es/[N]o)"),
+                utils.ExpectQuery(reply=""),
+                utils.ExpectMsg(">>> Is superuser? ([Y]es/[N]o)"),
+                utils.ExpectQuery(reply=""),
+                utils.ExpectMsg(">>> Profile? ([I]ntern/[F]ull-Time/[C]ontractor)"),
+                utils.ExpectQuery(reply='f'),
+                utils.ExpectMsg(""),
+                utils.ExpectMsg("=== Summary ==="),
+                utils.ExpectMsg("Name:           John Doe"),
+                utils.ExpectMsg("Email:          john.doe@example.com"),
+                utils.ExpectMsg("Is Staff:       True"),
+                utils.ExpectMsg("Is superuser:   False"),
+                utils.ExpectMsg("Profile:        fulltime"),
+                utils.ExpectMsg(">>> Confirm? ([Y]es/[N]o)"),
+                utils.ExpectQuery(reply=''),
+                utils.ExpectMsg(""),
+                utils.ExpectMsg("`ComplexForm` has been submitted:"),
+                utils.ExpectRe(r'  {.*}'),
+            ],
+        )
+        self.assertEqual({
+            'email': 'john.doe@example.com',
+            'name': "John Doe",
+            'is_staff': True,
+            'is_superuser': False,
+            'profile': 'fulltime',
         }, self.prompter.data)
